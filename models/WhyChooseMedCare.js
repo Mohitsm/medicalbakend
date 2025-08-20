7a124dd6c010d4986210e44ef0b230fba81ee231
Mohit <mohitsinghr124@gmail.com>
2025-08-20 21:00:25 +0530

    ghgxsj

--- models/WhyChooseMedCare.js
+++ models/WhyChooseMedCare.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
-const whyChooseMedCareSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true }, // store image path or base64
-}, { timestamps: true });
+const whyChooseMedCareSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-export const WhyChooseMedCare = mongoose.model("WhyChooseMedCare", whyChooseMedCareSchema);
+export const WhyChooseMedCare = mongoose.model(
+  "WhyChooseMedCare",
+  whyChooseMedCareSchema
+);
