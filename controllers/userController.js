88bf7a1d5dd45c63f1da7d0578bb1c0462ef52dc
Mohit <mohitsinghr124@gmail.com>
2025-08-20 21:54:02 +0530

    hhgh

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-import User from '../models/user.js';
+import User from '../models/User.js';
 import asyncHandler from 'express-async-handler';
 
 // @desc    Get all users with pagination and filtering
